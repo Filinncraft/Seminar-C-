566cbeea52ff85a611dee8c3b13b8707666ab95f
Mikhail Fedulov <fedulovmc@gmail.com>
2022-12-05 01:46:56 +0300

    Seminar 1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая будет говорить вам ваш возраст через шесть лет.
+// Console.WriteLine("How old are you?");
+// int test = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("After 6 year you will be a "+(test+6)+" years old");
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат.
+// Console.Write("Enter your number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Square of number {numb} is {numb*numb}");
+
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое квадратом второго.
+// Console.Write("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// if(numberA == numberB*numberB)
+//     Console.WriteLine("Ура, вы угадали!");
+// else 
+//     Console.WriteLine("Не квадрат!");
+
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number == 1)
+//     Console.WriteLine("Понедельник");
+//     else if(number == 2)
+//     Console.WriteLine("Вторник");
+//     else if(number == 3)
+//     Console.WriteLine("Среда");
+//     else if(number == 4)
+//     Console.WriteLine("Четверг");
+//     else if(number == 5)
+//     Console.WriteLine("Пятница");
+//     else if(number == 6)
+//     Console.WriteLine("Суббота");
+//     else if(number == 7)
+//     Console.WriteLine("Воскресенье");
+// else 
+//     Console.WriteLine("Такого дня нет");
+
+// Напишите программу, которая на вход принимает число N, а на выходе показывает все целые числа в промежутке от -N до N
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = number*(-1);
+// while (number1 <= number)
+// {
+//         Console.Write(number1+" ");
+//         number1++;
+// }
+
+// Напишите программу, которая на вход принимает трехзначное число, а на выходе показывает последнюю цифру числа.
+// Console.Write("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number>99 && number<1000)
+// {
+//     int number1 = number%10;
+//     Console.Write(number1);
+// }
+// else 
+//     Console.Write ("Число не трехзначное");
