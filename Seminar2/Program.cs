56244d127bdd8ef7874057797a17868682478167
Mikhail Fedulov <fedulovmc@gmail.com>
2022-12-05 02:26:16 +0300

    Homework after Seminar 1

---
+++ Seminar2/Program.cs
@@ -0,0 +1,66 @@
+﻿// void Test(int a, int b){
+//     if (a>b){
+//         Console.WriteLine ($"{a} больше {b}");
+//     }
+//     else{
+//         Console.WriteLine ($"{b} больше {a}");
+//     }    
+// }
+
+// int num1 = 4;
+// int num2 = 5;
+// Test(num1, num2);
+
+// int Test (int a, int b){
+//     if (a>b)
+//         return a;
+//     else
+//         return b;
+// }
+
+// int num1 = 5;
+// int num2 = 4;
+
+// int result = Test(num1, num2);
+// Console.WriteLine($"Результат - {result}");
+
+
+// int Line(){
+//     int num = new Random().Next(10,100);
+//     Console.WriteLine($"Ваше число - {num}");
+//     int dec = num/10;
+//     int ed = num%10;
+//     if (dec > ed)
+//         return dec;
+//     else if (dec == ed)
+//         return ed;
+//     else
+//         return ed;
+// }
+
+// int res = Line();
+// Console.WriteLine($"Большее число - {res}");
+
+
+// void Line(){
+//     int num = new Random().Next(100,1000);
+//     Console.WriteLine($"Ваше число - {num}");
+//     int dec = num/100;
+//     int ed = num%10;
+    
+
+//     Console.WriteLine($"Answer {dec}{ed}");
+// }
+
+// void Test(int num1, int num2){
+//     if (num1==num2*num2) 
+//         Console.WriteLine ("Является квадратом");
+//     else if (num2==num1*num1)
+//         Console.WriteLine ("Является квадратом");
+//     else{
+//         Console.WriteLine ("Не является квадратом");  
+// }
+// Console.WriteLine("введите первое число");
+// int num1 = Console.ReadLine
+// int num2 = 16;
+// Test(num1, num2);
---
+++ SeminarHW/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдает, какое большее, а какое меньшее.
+
+// Console.Write("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// if(numberA > numberB)
+//     Console.WriteLine($"{numberA} больше {numberB}");
+// else 
+//     Console.WriteLine($"{numberB} больше {numberA}");
+
+
+
+// Задача 4. Напишите программу, которая принимает на вход три числа и выдает максиальное из этих чисел.
+
+// Console.Write("Введите первое число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int C = Convert.ToInt32(Console.ReadLine());
+// if(A > B & A > C)
+//     Console.WriteLine($"{A} больше всех");
+// else if(A > B & A < C)
+//     Console.WriteLine($"{C} больше всех");
+// else if(A < B & A > C)
+//     Console.WriteLine($"{B} больше всех");
+// else if(A < B & A < C & B < C)
+//     Console.WriteLine($"{C} больше всех");
+// else
+//     Console.WriteLine($"{B} больше всех");
+
+
+// Задача 6. Напишите программу, которая на вход принимает число и выдает, является ли число четным.
+
+// Console.Write("Введите число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// if(numberA%2 == 0)
+//     Console.WriteLine($"{numberA} четное");
+// else 
+//     Console.WriteLine($"{numberA} не четное");
+
+
+// Задача 8. Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (count <= number)
+// {
+//         if(count%2 == 0)
+//         Console.Write(count+" ");
+//         count++;
+// }
