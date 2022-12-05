7846f12f184974f9f6f135e44ef98788fa1df8dc
Mikhail Fedulov <fedulovmc@gmail.com>
2022-12-05 14:44:49 +0300

    Homework after Seminar 2

--- SeminarHW/Program.cs
+++ SeminarHW/Program.cs
@@ -52,3 +52,47 @@
 //         Console.Write(count+" ");
 //         count++;
 // }
+
+
+// Задача 10. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+// int Line(){
+//     int num = new Random().Next(100,1000);
+//     Console.WriteLine($"Ваше число - {num}");
+//     int dec = num/10 % 10;
+//     return (dec);
+// }    
+// int res = Line();
+// Console.WriteLine(res);
+
+
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// void Test(int num){
+//     if (num < 100)
+//             Console.WriteLine ($"Третьей цифры нет");
+//     else
+//     {
+//         while(num > 999)
+//         {
+//             num = num / 10;
+//         }
+//         int res = num % 10;
+//         Console.WriteLine ($"Третья цифра в числе {res}");
+//     }
+// }
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Test(num);
+
+
+// Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number == 6 || number == 7)
+//     Console.WriteLine("Выходной");
+//     else if(number < 1 || number > 7)
+//     Console.WriteLine("Нет такого дня");
+// else 
+//     Console.WriteLine("Опять работать");
