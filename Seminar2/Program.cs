71b625e152415c89480c631c8c539e4cb271568c
Mikhail Fedulov <fedulovmc@gmail.com>
2022-12-05 03:29:34 +0300

    Seminar 2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,4 +1,6 @@
-﻿// void Test(int a, int b){
+﻿// Невозвратный метод
+
+// void Test(int a, int b){
 //     if (a>b){
 //         Console.WriteLine ($"{a} больше {b}");
 //     }
@@ -6,10 +8,12 @@
 //         Console.WriteLine ($"{b} больше {a}");
 //     }    
 // }
-
 // int num1 = 4;
 // int num2 = 5;
 // Test(num1, num2);
+
+
+// Возвратный метод
 
 // int Test (int a, int b){
 //     if (a>b)
@@ -17,13 +21,13 @@
 //     else
 //         return b;
 // }
-
 // int num1 = 5;
 // int num2 = 4;
-
 // int result = Test(num1, num2);
 // Console.WriteLine($"Результат - {result}");
 
+
+// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
 
 // int Line(){
 //     int num = new Random().Next(10,100);
@@ -42,25 +46,59 @@
 // Console.WriteLine($"Большее число - {res}");
 
 
-// void Line(){
+// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру из этого числа.
+
+// int Line(){
 //     int num = new Random().Next(100,1000);
 //     Console.WriteLine($"Ваше число - {num}");
 //     int dec = num/100;
 //     int ed = num%10;
-    
+//     return ((dec*10) + ed);
+// }    
+// int res = Line();
+// Console.WriteLine(res);
 
-//     Console.WriteLine($"Answer {dec}{ed}");
-// }
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе чмсло кратным первому.
+// Если второе число не кратно первому, то программа выводит остаток от деления.
 
 // void Test(int num1, int num2){
-//     if (num1==num2*num2) 
-//         Console.WriteLine ("Является квадратом");
-//     else if (num2==num1*num1)
-//         Console.WriteLine ("Является квадратом");
-//     else{
-//         Console.WriteLine ("Не является квадратом");  
+//     if (num2 % num1 == 0)
+//     { 
+//         Console.WriteLine ($"Число {num2} кратно числу {num1}");
+//     }
+//     else
+//     {
+//         int res = num2 % num1;
+//         Console.WriteLine ($"Число {num2} не кратно числу {num1}. Остаток от деления {res}");
+//     }
 // }
-// Console.WriteLine("введите первое число");
-// int num1 = Console.ReadLine
-// int num2 = 16;
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Test(num1, num2);
+
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом второго
+
+// void Test(int num1, int num2){
+//     if (num2 == num1 * num1)
+//     { 
+//         Console.WriteLine ($"Является");
+//     }
+//     else if (num1 == num2 * num2)
+//     { 
+//         Console.WriteLine ($"Является");
+//     }
+//     else
+//     {
+//        Console.WriteLine ($"Не является");
+//     }
+// }
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 // Test(num1, num2);
