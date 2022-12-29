d7eb9abe391ebaf27cc09e55bb5eb08ea32c818e
Mikhail Fedulov <fedulovmc@gmail.com>
2022-12-29 14:18:14 +0300

    Homework after Seminar 8+

--- SeminarHW/Program.cs
+++ SeminarHW/Program.cs
@@ -716,65 +716,64 @@
 // Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
 // которая будет построчно выводить массив, добавляя индексы каждого элемента. Массив размером 2 x 2 x 2
 
-// Console.Write("Enter x: ");
-// int x = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Enter y: ");
-// int y = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Enter z: ");
-// int z = Convert.ToInt32(Console.ReadLine());
-// int[,,] array3D = new int[x, y, z];
-// void CreateArray(int[,,] array3d)
-// {
-//   int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
-//   int  number;
-//   for (int i = 0; i < temp.GetLength(0); i++)
-//   {
-//     temp[i] = new Random().Next(10, 100);
-//     number = temp[i];
-//     if (i >= 1)
-//     {
-//       for (int j = 0; j < i; j++)
-//       {
-//         while (temp[i] == temp[j])
-//         {
-//           temp[i] = new Random().Next(10, 100);
-//           j = 0;
-//           number = temp[i];
-//         }
-//           number = temp[i];
-//       }
-//     }
-//   }
-//   int count = 0; 
-//   for (int x = 0; x < array3D.GetLength(0); x++)
-//   {
-//     for (int y = 0; y < array3D.GetLength(1); y++)
-//     {
-//       for (int z = 0; z < array3D.GetLength(2); z++)
-//       {
-//         array3D[x, y, z] = temp[count];
-//         count++;
-//       }
-//     }
-//   }
-// }
-// void ShowArray (int[,,] array3d)
-// {
-//   for (int i = 0; i < array3D.GetLength(0); i++)
-//   {
-//     for (int j = 0; j < array3D.GetLength(1); j++)
-//     {
-//     //   Console.Write($"X({i}) Y({j}) ");
-//       for (int k = 0; k < array3D.GetLength(2); k++)
-//       {
-//         Console.Write( $"{array3D[i,j,k]} ({i},{j},{k}) ");
-//       }
-//       Console.WriteLine();
-//     }
-//   }
-// }
-// CreateArray(array3D);
-// ShowArray(array3D);
+Console.Write("Enter x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] array3D = new int[x, y, z];
+void CreateArray(int[,,] array3d)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+void ShowArray (int[,,] array3d)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"{array3D[i,j,k]} ({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
+CreateArray(array3D);
+ShowArray(array3D);
 
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
